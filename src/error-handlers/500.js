f728bb4cf02c0ee26d475072795a383e578c8775
Ellis Yoo <yjoo.eyoo@gmail.com>
2021-04-21 16:57:42 -0700

    undo previos commit

--- src/error-handlers/500.js
+++ src/error-handlers/500.js
@@ -1,6 +1,6 @@
 'use strict'
 
 module.exports = (err,req,res,next)=>{
-    res.status(500).redirect('https://http.cat/500')
-    // res.status(500).send('Something went wrong')
+    // res.status(500).redirect('https://http.cat/500')
+    res.status(500).send('Something went wrong')
 }
